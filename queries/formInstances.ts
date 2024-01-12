1136aa4b1ab71f4df5e14838fa81361343e3c447
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-12 15:18:33 +0100

    some small style changes

--- queries/formInstances.ts
+++ queries/formInstances.ts
@@ -49,15 +49,18 @@ export const getFormPrefix = async function (formTtl: string) {
   const defaultPrefix = 'http://data.lblod.info/form-data/instances';
 
   const bindings = await bindingStream.toArray();
-  if (bindings.length) {
-    const binding = bindings[0].get('o');
-    if (binding) {
-      return binding.value;
-    } else {
-      return defaultPrefix;
-    }
-  } else {
+  if (bindings.length === 0) {
     return defaultPrefix;
+  }
+
+  const binding = bindings[0].get('o');
+
+  if (!binding || binding.value.length < 1) {
+    throw new Error(
+      'The form definition you tried to access defines an invalid form prefix!',
+    );
+  } else {
+    return binding.value;
   }
 };
 
