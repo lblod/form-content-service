1e391a301a92c2bf2b5c245d20f45bbefa806dcf
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-17 15:54:54 +0100

    fix bug

--- controllers/form-definitions.ts
+++ controllers/form-definitions.ts
@@ -4,8 +4,8 @@ import { fetchFormDefinition } from '../services/form-definitions';
 const formDefinitionRouter = express.Router();
 
 formDefinitionRouter.get('/:id', async function (req, res) {
-  const { formTtl, metaTtl } = await fetchFormDefinition(req.params.id);
-  res.send({ formTtl, metaTtl });
+  const { formTtl, metaTtl, prefix } = await fetchFormDefinition(req.params.id);
+  res.send({ formTtl, metaTtl, prefix });
 });
 
 export { formDefinitionRouter };
