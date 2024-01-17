e4ec3162bdaf7e76ea2bb23c784bcfd3ddc84ecb
Ruben Bottu <ruben.bottu@qmino.com>
2024-01-17 14:43:37 +0100

    Moved routes to directory

--- app.ts
+++ app.ts
@@ -1,28 +1,8 @@
-import { app, errorHandler, query } from 'mu';
+import { app, errorHandler } from 'mu';
 import Router from 'express-promise-router';
-import {
-  computeInstanceDeltaQuery,
-  fetchFormDefinitionById,
-  fetchFormInstanceById,
-  loadFormsFromConfig,
-} from './form-repository';
-import {
-  buildFormDeleteQuery,
-  cleanAndValidateFormInstance,
-} from './form-validator';
-import {
-  getFormLabel,
-  getFormInstances,
-  getFormPrefix,
-} from './queries/formInstances';
-import {
-  HttpError,
-  addTripleToTtl,
-  executeQuery,
-  fetchInstanceIdByUri,
-  fetchInstanceUriById,
-  ttlToInsert,
-} from './utils';
+import { loadFormsFromConfig } from './form-repository';
+import { formDefinitionRouter } from './controllers/form-definitions';
+import { formInstanceRouter } from './controllers/form-instances';
 
 const router = Router();
 
@@ -34,141 +14,8 @@ router.get('/', async function (_req, res) {
   res.send({ status: 'ok' });
 });
 
-router.get('/:id', async function (req, res) {
-  const form = await fetchFormDefinitionById(req.params.id);
-  if (!form) {
-    res.send(404);
-    return;
-  }
-  const prefix = await getFormPrefix(form.formTtl);
-  res.send({ formTtl: form.formTtl, metaTtl: form.metaTtl, prefix });
-});
+app.use('/', formDefinitionRouter);
 
-router.post('/:id', async function (req, res) {
-  const form = await fetchFormDefinitionById(req.params.id);
-  if (!form) {
-    res.send(404);
-    return;
-  }
-  // fetch form content from body
-  const { contentTtl, instanceUri } = req.body;
-
-  const validatedContent = await cleanAndValidateFormInstance(
-    contentTtl,
-    form,
-    instanceUri,
-  );
-
-  const formLabel = await getFormLabel(form.formTtl);
-  if (!formLabel) {
-    res.send(500);
-    return;
-  }
-  const predicate = 'http://mu.semte.ch/vocabularies/ext/label';
-  const updatedContent = addTripleToTtl(
-    validatedContent,
-    instanceUri,
-    predicate,
-    formLabel,
-  );
-
-  await query(ttlToInsert(updatedContent));
-
-  const id = await fetchInstanceIdByUri(instanceUri);
-
-  res.send({ id });
-});
-
-const fetchInstanceAndForm = async function (formId: string, id: string) {
-  const form = await fetchFormDefinitionById(formId);
-  if (!form) {
-    throw new HttpError('Form not found', 404);
-  }
-  const instance = await fetchFormInstanceById(form, id);
-
-  if (!instance) {
-    throw new HttpError('Instance not found', 404);
-  }
-  return { form, instance };
-};
-
-router.get('/:formId/instances', async function (req, res, next) {
-  const form = await fetchFormDefinitionById(req.params.formId);
-  if (!form) {
-    res.send(404);
-    return;
-  }
-
-  const formLabel = await getFormLabel(form.formTtl);
-  if (!formLabel) {
-    res.send(500);
-    return;
-  }
-
-  const formInstances = await getFormInstances(formLabel, next);
-
-  res.send(formInstances);
-});
-
-router.get('/:id/instances/:instanceId', async function (req, res) {
-  const { instance } = await fetchInstanceAndForm(
-    req.params.id,
-    req.params.instanceId,
-  );
-  res.send(instance);
-});
-
-router.put('/:id/instances/:instanceId', async function (req, res) {
-  const instanceId = req.params.instanceId;
-  const { form, instance } = await fetchInstanceAndForm(
-    req.params.id,
-    instanceId,
-  );
-
-  const validatedContentTtl = await cleanAndValidateFormInstance(
-    req.body.contentTtl,
-    form,
-    instance.instanceUri,
-  );
-
-  const deltaQuery = await computeInstanceDeltaQuery(
-    instance.formDataTtl,
-    validatedContentTtl,
-  );
-
-  if (!deltaQuery) {
-    res.send({ instance });
-    return;
-  }
-
-  await query(deltaQuery);
-
-  const newInstance = await fetchFormInstanceById(form, instanceId);
-
-  res.send({ instance: newInstance });
-});
-
-router.delete('/:id/instances/:instanceId', async function (req, res, next) {
-  const form = await fetchFormDefinitionById(req.params.id);
-  if (!form) {
-    res.send(404);
-    return;
-  }
-
-  const instanceUri = await fetchInstanceUriById(req.params.instanceId);
-  if (!instanceUri) {
-    res.send(404);
-    return;
-  }
-
-  // Delete form instance based on form definition.
-  const query = await buildFormDeleteQuery(form.formTtl, instanceUri);
-  await executeQuery(query, next);
-
-  // TODO at this stage inverse relations are kept intact even if the object gets deleted.
-  // Would be better to replace this relation with a tombstone relation.
-
-  res.send(200);
-});
+app.use('/', formInstanceRouter);
 
 app.use(errorHandler);
