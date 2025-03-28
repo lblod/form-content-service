8b36195d00e8debe545c4d9846f445dbd9b7d609
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-28 15:14:53 +0100

    lmb-1218: remove the usages of the form definition

--- services/form-definitions.ts
+++ services/form-definitions.ts
@@ -139,3 +139,34 @@ export const findFormUsages = async (formId: string) => {
 
   return Array.from(new Set(results.map((b) => b.usage?.value)));
 };
+
+export const removeFormDefinitionUsage = async (formId: string) => {
+  const queryString = `
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX form: <http://lblod.data.gift/vocabularies/forms/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+
+    DELETE {
+      GRAPH ?g {
+        ?instance ?p ?o .
+      }
+    }
+    WHERE {
+      GRAPH ?g {
+        ?form mu:uuid ${sparqlEscapeString(formId)}.
+        ?definition form:targetType ?targetType .
+
+        ?instance a ?targetType .
+        ?instance ?p ?o .
+      }
+    }
+  `;
+  try {
+    await update(queryString);
+  } catch (error) {
+    throw new HttpError(
+      `Something went wrong while removing the form with usages (${formId})`,
+      500,
+    );
+  }
+};
