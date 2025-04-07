d367e839309197dd0538457ce958e5a2ae1ea9da
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-07 12:09:49 +0200

    lmb-1502: fetch the form options for type

--- controllers/custom-forms.ts
+++ controllers/custom-forms.ts
@@ -3,22 +3,44 @@ import { Request, Response } from 'express';
 
 import { fetchFormDirectoryNames } from '../services/forms-from-config';
 import { fetchCustomFormTypes } from '../services/custom-forms';
+import { getInstancesForForm } from '../services/form-instances';
 
 export const customFormRouter = Router();
 
-customFormRouter.get('/form-types', async (req: Request, res: Response) => {
-  const defaultTypeIds = await fetchFormDirectoryNames();
-  const customTypes = await fetchCustomFormTypes();
+customFormRouter.get(
+  '/form-type-options',
+  async (req: Request, res: Response) => {
+    const defaultTypeIds = await fetchFormDirectoryNames();
+    const customTypes = await fetchCustomFormTypes();
 
-  return res.status(200).send({
-    defaultTypes: defaultTypeIds.map((id) => {
-      return {
-        typeId: id,
-        label: id,
-      };
-    }),
-    customTypes,
-  });
-});
-customFormRouter.get('/:form-type-id/forms', async (req: Request, res: Response) => { });
-customFormRouter.get('/:formId/summary-fields', async (req: Request, res: Response) => { });
+    return res.status(200).send({
+      defaultTypes: defaultTypeIds.map((id) => {
+        return {
+          typeId: id,
+          label: id,
+        };
+      }),
+      customTypes,
+    });
+  },
+);
+customFormRouter.get(
+  '/:formTypeId/form-options',
+  async (req: Request, res: Response) => {
+    const form = await getInstancesForForm(req.params.formTypeId, {});
+    const labelProperty = form.labels[0].name;
+    return res.status(200).send({
+      form,
+      forms: form.instances.map((instance) => {
+        return {
+          id: instance.id,
+          label: instance[labelProperty] ?? instance.id,
+        }
+      })
+    });
+  },
+);
+customFormRouter.get(
+  '/:formId/summary-fields',
+  async (req: Request, res: Response) => { },
+);
